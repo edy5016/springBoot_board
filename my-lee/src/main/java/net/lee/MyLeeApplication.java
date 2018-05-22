74910377cca210d4f06e030533a4e16e5f9e5e5a
lee <edy5016@navet.com>
2018-05-23 01:03:28 +0900

    s

---
+++ my-lee/src/main/java/net/lee/MyLeeApplication.java
@@ -0,0 +1,12 @@
+package net.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLeeApplication.class, args);
+	}
+}
